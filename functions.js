82bd54f3ddefcbf084d64a9b92a2198846ad8c62
Michael K <michaelkz@tyfonmail.se>
2021-11-08 14:55:20 +0100

    Created and added loadscreen to portfolio

---
+++ functions.js
@@ -0,0 +1,5 @@
+//function to hide loadscreen when it's done
+function noOverflow() {
+  document.getElementById('checker').style.overflowY = 'scroll'
+  document.getElementById('bigContainer').style.display = 'none'
+}
